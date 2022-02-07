0d84daeb9809f0d4b0f0eb1bea302de3bb8fbea4
David Amador <54488581+dsamador@users.noreply.github.com>
2022-02-07 16:24:18 -0500

    Generic classes

---
+++ Genericos17Clases/CPunto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genericos17Clases
+{
+    //T representa al tipo con el que se trabaja en un momento dado
+    public class CPunto<T>
+    {
+        //creamos atributos de tipo T
+        private T x;
+        private T y;
+
+        //podemos recibir parametros de tipo T
+        public CPunto(T px, T py)
+        {
+            x = px;
+            y = py;
+        }
+
+        public override string ToString()
+        {
+            return $"X={x}, Y={y}";
+        }
+
+        public void Reset()
+        {
+            // colocamos el valor de default para el tipo T
+            // numericos 0
+            // referencias null lo que incluye a los strings
+            x = default(T);
+            y = default(T);
+        }
+
+        public void EncuentraTipo()
+        {
+            if(typeof(T) == typeof(int))
+                Console.WriteLine("Trabajo como entero");
+            else
+                Console.WriteLine("Soy de otro tipo");
+        }
+    }
+}
---
+++ Genericos17Clases/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Genericos17Clases
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Para puntoI, T trabaja como int
+            CPunto<int> puntoI = new CPunto<int>(3, 4);
+
+            //para puntoD, T trabaja como double
+            CPunto<double> puntoD = new CPunto<double>(3.3, 4.8);
+            
+            //Para puntoF T trabaja como float
+            CPunto<float> puntoF = new CPunto<float>(3.33f, 4.84f);
+
+            Console.WriteLine(puntoI);
+            Console.WriteLine(puntoD);
+            Console.WriteLine(puntoF);
+            
+            //colocamos el valor de default
+            puntoI.Reset();
+            Console.WriteLine(puntoI);
+
+            //verificamos si trabaja como entero
+            puntoI.EncuentraTipo();
+            puntoD.EncuentraTipo(); 
+        }
+    }
+}
