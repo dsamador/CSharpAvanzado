4683861ab2fd867f19bf8f3f86ac5458d7eac221
David Amador <54488581+dsamador@users.noreply.github.com>
2022-02-08 18:01:07 -0500

    Uso de Func

--- DelegadosHdL/Program.cs
+++ DelegadosHdL/Program.cs
@@ -5,40 +5,31 @@ using System.Threading.Tasks;
 namespace DelegadosHdL
 {
     internal class Program
-    {
-        public delegate void Mostrar(string cadena);
+    {        
         static async Task Main(string[] args)
         {
             //El ultimo parametro es lo que se regresa, si tiene uno solo
             //regresa ese tipo, si tiene dos o mas todos menos el ultimo
             //son parametros que recibe, Func debe tener al menos el tipo de 
             //regreso
-            Func<string, int> mostrar = Show;
-            Mostrar mostrar2 = Show;
+            Func<string, int> mostrar = Show;            
             HacerAlgo(mostrar);
         }
 
-        public static void HacerAlgo(Mostrar funcionFinal)
+        public static void HacerAlgo(Func<string, int> funcionFinal)
         {
             Console.WriteLine("Hago algo");
             funcionFinal("se envio desde otra funcion");
             Console.WriteLine("Hago algo mas");
-        }
-        public static void HacerAlgo2(Func<string, int> funcionFinal)
-        {
-            Console.WriteLine("Hago algo");
-            funcionFinal("se envio desde otra funcion");
-            Console.WriteLine("Hago algo mas");
-        }
+        }      
 
-        public static void Show(string cad)
-        {
-            Console.WriteLine("Hola soy un delegado" + cad);
-        }
+      
 
-        public static int ShowLenCad(string cad)
+        public static int Show(string cad)
         {
             return cad.Count();
         }
+
+        
     }
 }
