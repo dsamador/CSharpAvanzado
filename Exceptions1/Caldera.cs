d53f121347ef8e639d4d58bc9ba6719deb361569
David Amador <54488581+dsamador@users.noreply.github.com>
2022-03-05 19:38:00 -0500

    Caldera Updated

---
+++ Exceptions1/Caldera.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions1
+{
+    public class Caldera
+    {
+        public int TempMax { get; } = 120;
+        public int TempActual { get; set; } = 0;
+        public string Marca { get; set; } = "";
+        public bool Funciona { get; set; } = true;
+
+        public Caldera(int tempActual, string marca)
+        {
+            TempActual = tempActual;
+            Marca = marca;
+        }
+        //Sin excepciones
+        //public void Trabajar(int Aumento)
+        //{
+        //    if(Funciona == false)
+        //        Console.WriteLine($"La caldera {Marca} esta descompuesta");
+        //    else
+        //    {
+        //        TempActual += Aumento;
+        //        Console.WriteLine($"La temperatura actual es de {TempActual}");
+        //        if(TempActual > TempMax)
+        //        {
+        //            Console.WriteLine($"{Marca} supero la temperatura, " +
+        //                $"tiene {TempActual} ");
+        //            TempActual = TempMax;
+        //            Funciona = false;
+        //        }
+        //    }            
+        //}
+
+        public void Trabajar(int Aumento)
+        {
+            if (Funciona == false)
+                Console.WriteLine($"La caldera {Marca} esta descompuesta");
+            else
+            {
+                TempActual += Aumento;
+                Console.WriteLine($"La temperatura actual es de {TempActual}");
+                if (TempActual > TempMax)
+                {
+                    Console.WriteLine($"{Marca} supero la temperatura, " +
+                        $"tiene {TempActual} ");
+                    TempActual = TempMax;
+                    Funciona = false;
+
+                    throw new Exception($"La caldera {Marca} se sobrecalienta");
+                }
+            }
+        }
+    }
+}
