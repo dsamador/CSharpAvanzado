f2279c94382a51774e15bf25e1b28c9a95ce0ed1
David Amador <54488581+dsamador@users.noreply.github.com>
2022-03-15 19:26:48 -0500

    HashTable 1

---
+++ 5HashTable/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+
+namespace _5HashTable
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable miTabla = new Hashtable();
+
+            //Los valores pueden ser cualquier cosa, literalmente
+
+            miTabla.Add(123, "Hola");
+            miTabla.Add(234, "Saludos");
+            miTabla.Add(45, "Adios");
+            miTabla.Add(567, "C#");
+            miTabla.Add(12763, "Programacion");
+
+            //Mostramos los elementos
+            foreach (DictionaryEntry item in miTabla)
+            {
+                Console.WriteLine($"{item.Key}, {item.Value}");
+            }
+
+            //Intentamos colocar un elemento con llave repetida
+            //miTabla.Add(123, "Otro mas");
+
+            //Cantidad de elementos
+            Console.WriteLine(miTabla.Count);
+
+            //Obtenemos el elemento de determinada llave
+            Console.WriteLine(miTabla[567]);
+            Console.WriteLine(miTabla[777]);//vacio
+        }
+    }
+}
