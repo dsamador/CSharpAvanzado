d71585f9211fed765ea06170067b7f41c7b8c798
David Amador <54488581+dsamador@users.noreply.github.com>
2022-06-15 19:39:06 -0500

    Método que devuelve un ref locals

---
+++ Eventos y Delegados Cs28/AsistenteDescarga.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace Eventos_y_Delegados_Cs28
+{
+    public class AsistenteDescarga
+    {
+        //Paso 1> crear el delegado
+        public delegate void GestorEventoArchivoDescargado(object fuente, EventArgs args);
+        
+        //Paso 2> crear el evento basado en el delegado
+        public event GestorEventoArchivoDescargado ArchivoDescargado;
+
+        //Paso 3> Iniciar el evento
+        protected virtual void EnArchivoDescargado()
+        {
+            if(ArchivoDescargado != null)
+            {
+                ArchivoDescargado(this, EventArgs.Empty);
+            }
+        }
+
+        public void Descarga(Archivo archivo)
+        {
+            Console.WriteLine("Descargando...");
+            Thread.Sleep(4000);
+
+            //Paso 3.1
+            EnArchivoDescargado();
+        }
+    }
+}
--- Eventos y Delegados Cs28/Program.cs
+++ Eventos y Delegados Cs28/Program.cs
@@ -1,1 +1,7 @@
 ﻿
+using Eventos_y_Delegados_Cs28;
+
+var archivo = new Archivo() { Titulo = "Archivo 1" };
+var asistenteDescarga = new AsistenteDescarga();//emisor
+
+asistenteDescarga.Descarga(archivo);
